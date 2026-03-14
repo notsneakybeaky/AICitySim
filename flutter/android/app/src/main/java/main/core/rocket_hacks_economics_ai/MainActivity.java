a0044147ca4cfa42ce5073b67f35b5280a9f4f0d
itshaithamn <hnaksho@oakland.edu>
2026-03-14 16:40:40 -0400

    first commit

--- flutter/android/app/src/main/java/main/core/rocket_hacks_economics_ai/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package main.core.rocket_hacks_economics_ai;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- flutter/android/app/src/main/kotlin/main/core/rocket_hacks_economics_ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package main.core.rocket_hacks_economics_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
